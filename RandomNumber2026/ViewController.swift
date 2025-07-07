1113aec2b44a7f589aae9c8c13648e6fa9622152
Yuri Luka <lukayurii.work@gmail.com>
2025-07-07 19:57:04 +0500

    Work with main screen

--- RandomNumber2026/ViewController.swift
+++ RandomNumber2026/ViewController.swift
@@ -8,12 +8,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    // MARK: - IBOutlets
+    @IBOutlet var minimumValueLabel: UILabel!
+    @IBOutlet var maximumValueLabel: UILabel!
+    @IBOutlet var randomNumberLabel: UILabel!
+    @IBOutlet var getResultButtonLabel: UIButton!
+    
+    // MARK: - Override Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func getResultButtonDidTapped() {
+    }
+    
 }
 
